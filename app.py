31a3864682c14727d647b83a6428366ac89188d2
William Dula <wsdula@gmail.com>
2023-10-05 20:40:02 -0400

    Got tutorial app working

--- app.py
+++ app.py
@@ -1,18 +1,39 @@
-from flask import Flask
+# app.py
+
+from flask import Flask, render_template, request
 from flask_assets import Bundle, Environment
+
+from todos import todos
 
 app = Flask(__name__)
 
 assets = Environment(app)
 css = Bundle("src/main.css", output="dist/main.css")
+js = Bundle("src/*.js", output="dist/main.js")
 
 assets.register("css", css)
+assets.register("js", js)
 css.build()
+js.build()
 
 
 @app.route("/")
 def homepage():
     return render_template("index.html")
+
+@app.route("/search", methods=["POST"])
+def search_todo():
+    search_term = request.form.get("search")
+
+    if not len(search_term):
+        return render_template("todo.html", todos=[])
+
+    res_todos = []
+    for todo in todos:
+        if search_term in todo["title"]:
+            res_todos.append(todo)
+
+    return render_template("todo.html", todos=res_todos)
 
 if __name__ == "__main__":
     app.run(debug=True)
