38a0a157f6dc9d9181a06ccb5d4af5bb8f964c9a
William Dula <wsdula@gmail.com>
2023-10-13 22:10:09 -0400

    got tags working with hyperscript

--- app.py
+++ app.py
@@ -8,47 +8,26 @@ app = Flask(__name__)
 
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-tagsList = []
-
 @app.route('/')
 def index():
-    tagsList = []
     return render_template('index.html')
-
-@app.route('/tags', methods=['POST','DELETE'])
-def tags():
-    if request.method == 'DELETE':
-        tagText = request.form['tag']
-        app.logger.info(tagText)
-
-        tagsList.remove(tagText)
-        logtext = tagText + " has been removed from list: " + str(tagsList)
-        app.logger.info(logtext)
-        result = ''
-    else:
-        #Take in a tag, trim it, then push it back in it's respective html element
-        tagText = request.form['hashtags'].strip().lower()
-        result = '''<div id=tag class="m-2 w-auto font-medium text-sm pointer-events-auto bg-[#242424] text-white p-2.5 text-center rounded-3xl cursor-pointer hover:bg-gray-600" hx-delete="/tags" hx-target="this" hx-on::after-request="this.remove()">'''+tagText+'</div>'
-
-        tagsList.append(str(tagText))
-        logtext = tagText + " has been added to list: " + str(tagsList)
-        app.logger.info(logtext)
-
-    return result
-
 
 @app.route('/submitIdea', methods=['POST'])
 def submitIdea():
-    # file = request.files['file']
-    # text = request.form['notes']
+    idea = {
+            'file' : request.files['file'],
+            'notes' : request.form['notes'],
+            'tagsList' : request.form['taglist']
+        }
 
-    a = jsonify(request.form['hashtags'])
+    #TODO: Create backend to push data to after capture
 
     # Process the file and text here
+    app.logger.info(idea)
 
     result = 'Form submitted successfully!'
 
-    return a
+    return result
 
 if __name__ == '__main__':
     app.run(debug=True)
